f959f278b97aa70cf5f8a78992864eb0c45fc1b5
Jonasoft <jonathan.monroy@live.com>
2024-10-26 20:16:58 -0300

    CreateProduct

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: 'postgres',
+      password: 'admin',      
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductsModule,
+  ]
 })
 export class AppModule {}
