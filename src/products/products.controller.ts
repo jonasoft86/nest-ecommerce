fada53cb209af63d86c44915d7e674b0b5522ee7
Jonasoft <jonathan.monroy@live.com>
2024-11-11 21:54:26 -0300

    DocApi

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,13 +7,19 @@ import { Auth } from 'src/auth/decorators/auth.decorator';
 import { ValidRoles } from 'src/auth/interfaces';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { User } from 'src/auth/entities/user.entity';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Product } from './entities';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
   @Auth(ValidRoles.admin)
+  @ApiResponse({ status: 201, description: 'Product was created', type: Product  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 403, description: 'Forbidden. Token related.' })
   create(
     @Body() createProductDto: CreateProductDto,
     @GetUser() user:User,
